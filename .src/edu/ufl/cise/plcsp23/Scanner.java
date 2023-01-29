614f3e3d3f9fdbc9a8b80eca20b7063b985935df
YaaseenM <92874301+YaaseenM@users.noreply.github.com>
2023-01-28 22:42:47 -0500

    Information from slides.

---
+++ .src/edu/ufl/cise/plcsp23/Scanner.java
@@ -0,0 +1,10 @@
+package edu.ufl.cise.plcsp23;
+
+public class Scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ .src/edu/ufl/cise/plcsp23/Token.java
@@ -0,0 +1,5 @@
+package edu.ufl.cise.plcsp23;
+
+public class Token {
+
+}
